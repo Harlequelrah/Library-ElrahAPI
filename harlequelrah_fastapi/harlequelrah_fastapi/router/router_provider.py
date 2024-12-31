9da7cdd28cc7b1ca69cd907172eca5a3a9bff7c9
Harlequelrah <degbovimax19@gmail.com>
2024-12-31 17:47:50 +0000

    ajout de la classe CustomProviderRouter

--- harlequelrah_fastapi/harlequelrah_fastapi/router/router_provider.py
+++ harlequelrah_fastapi/harlequelrah_fastapi/router/router_provider.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends
 from harlequelrah_fastapi.authentication.authenticate import Authentication
 from typing import List, Optional
 from harlequelrah_fastapi.crud.crud_model import CrudForgery
+from harlequelrah_fastapi.router.route_config import RouteConfig
 def provide_router(prefix:str,tags:List[str],PydanticModel,crud:CrudForgery,authentication:Optional[Authentication]=None,get_session:Optional[callable]=None)->APIRouter:
     router = APIRouter(
         prefix=prefix,
@@ -54,7 +55,7 @@ class ProvideRouter:
             tags=tags,
             dependencies=(
                 [
-                    Depends(authentication.get_access_token),
+                    Depends(authentication.get_access_token)  ,
                     Depends(authentication.get_session),
                 ]
                 if authentication
@@ -98,3 +99,122 @@ class ProvideRouter:
             @self.router.delete("/delete/{id}")
             async def delete(id:int):
                 return await self.crud.delete(id)
+
+
+class CustomProvideRouter:
+    def __init__(
+        self,
+        prefix: str,
+        tags: List[str],
+        PydanticModel,
+        crud: CrudForgery,
+        get_access_token: Optional[callable] = None,
+        get_session: callable = None,
+    ):
+        self.crud = crud
+        self.PydanticModel = PydanticModel
+        self.CreatePydanticModel = crud.CreatePydanticModel
+        self.UpdatePydanticModel = crud.UpdatePydanticModel
+        self.token_dependency=Depends(get_access_token) if get_access_token else Depends(None),
+        self.router = APIRouter(
+            prefix=prefix,
+            tags=tags,
+            dependencies= [
+                    Depends(get_session),
+                ]
+            ,
+        )
+
+    def initialize_router(self, init_data: List[RouteConfig]):
+        for config in init_data:
+            if config.router_name ==  "count" and config.is_activated:
+                @self.router.get(
+                        f"/count",
+                        dependencies=[
+                            (
+                                self.token_dependency
+                                if config.is_protected
+                                else Depends(None)
+                            )
+                        ]
+                    )
+                async def count():
+                    count = await self.crud.count()
+                    return {"count": count}
+
+            if config.router_name ==  "read-one" and config.is_activated:
+                @self.router.get(
+                    "/read-one/{id}",
+                    response_model=self.PydanticModel,
+                    dependencies=[
+                        (
+                            self.token_dependency
+                            if config.is_protected
+                            else Depends(None)
+                        )
+                    ],
+                )
+                async def read_one(id: int):
+                    return await self.crud.read_one(id)
+
+            if config.route_name=="read-all" and config.is_activated:
+                @self.router.get(
+                    "/read-all",
+                    response_model=List[self.PydanticModel],
+                    dependencies=[
+                        (self.token_dependency if config.is_protected else Depends(None))
+                    ],
+                )
+                async def read_all(skip: int = 0, limit: int = None):
+                    return await self.crud.read_all(skip=skip, limit=limit)
+
+        if config.route_name == "read-all-by-filter" and config.is_activated:
+
+            @self.router.get(
+                "/read-all-by-filter",
+                response_model=List[self.PydanticModel],
+                dependencies=[
+                    (self.token_dependency if config.is_protected else Depends(None))
+                ],
+            )
+            async def read_all_by_filter(
+                filter: str, value: str, skip: int = 0, limit: int = None
+            ):
+                return await self.crud.read_all_by_filter(
+                    skip=skip, limit=limit, filter=filter, value=value
+                )
+
+            if config.route_name == "create" and config.is_activated:
+
+                @self.router.post(
+                    "/create",
+                    response_model=self.PydanticModel,
+                    dependencies=[
+                        (self.token_dependency if config.is_protected else Depends(None))
+                    ],
+                )
+                async def create(create_obj: self.CreatePydanticModel):
+                    return await self.crud.create(create_obj)
+
+            if config.route_name=="update" and config.is_activated:
+
+                @self.router.put(
+                    "/update/{id}",
+                    response_model=self.PydanticModel,
+                    dependencies=[
+                        (self.token_dependency if config.is_protected else Depends(None))
+                    ],
+                )
+                async def update(id: int, update_obj: self.UpdatePydanticModel):
+                    return await self.crud.update(id, update_obj)
+
+            if  config.route_name=="delete" and config.is_activated:
+
+                @self.router.delete(
+                    "/delete/{id}",
+                    dependencies=[
+                        (self.token_dependency if config.is_protected else Depends(None))
+                    ],
+                )
+                async def delete(id: int):
+                    return await self.crud.delete(id)
