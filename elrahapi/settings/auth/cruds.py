bcf07fd50f637e55bac0d28dae7b372dfcc3ebaf
Harlequelrah <degbovimax19@gmail.com>
2025-03-27 23:59:36 +0000

    deplacement de l'authenfication dans settings

--- elrahapi/settings/auth/cruds.py
+++ elrahapi/settings/auth/cruds.py
@@ -4,7 +4,7 @@ from elrahapi.authorization.role_model import RoleCreateModel, RolePydanticModel
 from elrahapi.authorization.role_privilege_model import RolePrivilegeCreateModel, RolePrivilegePatchModel, RolePrivilegePydanticModel, RolePrivilegeUpdateModel
 from elrahapi.authorization.user_privilege_model import UserPrivilegePatchModel, UserPrivilegePydanticModel
 from elrahapi.crud.crud_forgery import CrudForgery
-from myproject.settings.database import authentication
+from myproject.settings.auth_configs import authentication
 from elrahapi.crud.crud_models import CrudModels
 from .models import User, UserPrivilege,Role,Privilege,RolePrivilege
 from .schemas import UserCreateModel,UserUpdateModel,UserPatchModel,UserPydanticModel
--- elrahapi/settings/auth_configs.py
+++ elrahapi/settings/auth_configs.py
@@ -1,13 +1,13 @@
-# from elrahapi.authentication.authentication_router_provider import AuthenticationRouterProvider
-# from .secret import MAX_ATTEMPT_LOGIN
-# from myproject.userapp.cruds import user_crud_models
-# from .database import authentication
+from elrahapi.authentication.authentication_router_provider import AuthenticationRouterProvider
+from .secret import MAX_ATTEMPT_LOGIN
+from .auth.cruds import user_crud_models
+from .database import authentication
 
 
-# user_crud_models.sqlalchemy_model.MAX_ATTEMPT_LOGIN = MAX_ATTEMPT_LOGIN
-# authentication.authentication_models=user_crud_models
-# authentication_router_provider = AuthenticationRouterProvider(authentication=authentication)
-# authentication_router = authentication_router_provider.get_auth_router()
+user_crud_models.sqlalchemy_model.MAX_ATTEMPT_LOGIN = MAX_ATTEMPT_LOGIN
+authentication.authentication_models=user_crud_models
+authentication_router_provider = AuthenticationRouterProvider(authentication=authentication)
+authentication_router = authentication_router_provider.get_auth_router()
 
 
 
