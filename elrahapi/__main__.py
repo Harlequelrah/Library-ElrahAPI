60196a628f8d62c872897526848dd7c5e373c5b1
Harlequelrah <degbovimax19@gmail.com>
2025-07-08 18:32:36 +0000

    modifications des scripts :
    - generer le projet , ajoute le nom du projet au .env
    
    - remplacement de get_project_folder par get_apps_dir

--- elrahapi/__main__.py
+++ elrahapi/__main__.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import sys
 import subprocess
+from typing import Optional
 
 from elrahapi.security.secret import define_algorithm_and_key
+
 
 def repline(file, line, line_content):
     with open(file, "r") as ficher:
@@ -13,64 +15,68 @@ def repline(file, line, line_content):
         ficher.write(line_content)
         ficher.writelines(a[line:])
 
-def update_env_with_secret_and_algorithm(env_file: str, algorithm: str = "HS256"):
-            algo, key = define_algorithm_and_key(algorithm=algorithm)
-            with open(env_file, "r") as f:
-                lines = f.readlines()
-            secret_key_line = None
-            algorithm_line = None
-            for idx, line in enumerate(lines):
-                if line.strip().startswith("SECRET_KEY"):
-                    secret_key_line = idx + 1
-                if line.strip().startswith("ALGORITHM"):
-                    algorithm_line = idx + 1
-            if secret_key_line:
-                repline(env_file, secret_key_line, f"SECRET_KEY={key}\n")
-            if algorithm_line:
-                repline(env_file, algorithm_line, f"ALGORITHM={algo}\n")
 
-def generate_secret_key(algorithm: str = "HS256") -> str:
+def update_env_with_secret_and_algorithm(env_file: str, algorithm: str = "HS256"):
+    algo, key = define_algorithm_and_key(algorithm=algorithm)
+    with open(env_file, "r") as f:
+        lines = f.readlines()
+    secret_key_line = None
+    algorithm_line = None
+    for idx, line in enumerate(lines):
+        if line.strip().startswith("SECRET_KEY"):
+            secret_key_line = idx + 1
+        if line.strip().startswith("ALGORITHM"):
+            algorithm_line = idx + 1
+    if secret_key_line:
+        repline(env_file, secret_key_line, f"SECRET_KEY={key}\n")
+    if algorithm_line:
+        repline(env_file, algorithm_line, f"ALGORITHM={algo}\n")
+
+
+def generate_secret_key(env_src_path:Optional[str]=None,algorithm: str = "HS256") -> str:
+    if env_src_path is None :
         project_folder = os.getcwd()
         env_src_path = os.path.join(project_folder, ".env")
-        update_env_with_secret_and_algorithm(env_src_path, algorithm)
-        print("SECRET_KEY and ALGORITHM have been generated and added to the .env file")
+    update_env_with_secret_and_algorithm(env_src_path, algorithm)
+    print("SECRET_KEY and ALGORITHM have been generated and added to the .env file")
 
 
 def startproject(project_name):
     project_path = os.path.join(os.getcwd(), project_name)
     os.makedirs(project_path, exist_ok=True)
-    sub_project_path = os.path.join(project_path, project_name)
-    os.makedirs(sub_project_path, exist_ok=True)
+    apps_dir = os.path.join(project_path, project_name)
+    os.makedirs(apps_dir, exist_ok=True)
 
     # Initialise le dépôt Git
-    try :
+    try:
         subprocess.run(["git", "init", project_path])
         print(f"Git repo initialized in {project_path}")
-    except Exception as e :
+    except Exception as e:
         print(f"Error initializing the Git repository: {e}")
 
-    subprocess.run(["alembic", "init","alembic"], cwd=project_path)
+    subprocess.run(["alembic", "init", "alembic"], cwd=project_path)
     print(f"Alembic has been initialized in {project_path}")
 
     with open(f"{project_path}/__init__.py", "w") as f:
         f.write("# __init__.py\n")
 
-    with open(f"{sub_project_path}/__init__.py", "w") as f:
+    with open(f"{apps_dir}/__init__.py", "w") as f:
         f.write("# __init__.py\n")
 
-    settings_path = os.path.join(sub_project_path, "settings")
+    settings_path = os.path.join(apps_dir, "settings")
     os.makedirs(settings_path, exist_ok=True)
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
     source_settings_path = os.path.join(script_dir, "settings")
     main_path_dir = os.path.join(script_dir, "main")
     main_script_src_path = os.path.join(main_path_dir, "main.py")
-    main_script_dest_path = os.path.join(sub_project_path, "main.py")
+    main_script_dest_path = os.path.join(apps_dir, "main.py")
     shutil.copyfile(main_script_src_path, main_script_dest_path)
     print(f"The file 'main.py' has been copied to {main_script_dest_path}")
 
     env_src_path = os.path.join(main_path_dir, ".env")
     env_dest_path = os.path.join(project_path, ".env")
+    print(f"{env_src_path,env_dest_path=}")
     shutil.copyfile(env_src_path, env_dest_path)
     print(f"The '.env' file has been copied to {env_dest_path}")
 
@@ -79,10 +85,20 @@ def startproject(project_name):
     shutil.copyfile(example_env_src_path, example_env_dest_path)
     print(f"The file '.env.example' has been copied to {example_env_dest_path}")
 
-    main_project_files_path = os.path.join(main_path_dir,"main_project_files")
+    with open(env_dest_path, "r") as f:
+        lines = f.readlines()
+    with open(env_dest_path, "w") as f:
+        for line in lines:
+            if line.strip().startswith("PROJECT_NAME="):
+                f.write(f"PROJECT_NAME={project_name}\n")
+            else:
+                f.write(line)
+    main_project_files_path = os.path.join(main_path_dir, "main_project_files")
     if os.path.exists(main_project_files_path):
         shutil.copytree(main_project_files_path, project_path, dirs_exist_ok=True)
-        print("The files .gitignore, __main__.py, and README.md have been copied successfully.")
+        print(
+            "The files .gitignore, __main__.py, and README.md have been copied successfully."
+        )
     else:
         print("The source folder 'main_project_files' was not found.")
 
@@ -94,12 +110,12 @@ def startproject(project_name):
     with open(os.path.join(project_path, "requirements.txt"), "w") as f:
         subprocess.run(["pip", "freeze"], stdout=f)
     print(f"The project {project_name} has been created successfully.")
-    generate_secret_key()
+    generate_secret_key(env_src_path=env_dest_path)
 
 
 def startapp(app_name):
-    project_folder = get_project_folder()
-    app_path = os.path.join(project_folder, app_name)
+    apps_dir_folder = get_apps_dir()
+    app_path = os.path.join(apps_dir_folder, app_name)
     os.makedirs(app_path, exist_ok=True)
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -111,36 +127,45 @@ def startapp(app_name):
     else:
         print("The 'sqlapp' folder was not found.")
 
-
-def get_project_folder():
+def get_apps_dir():
     parent_dir = os.getcwd()
-    project_folders = [
-        f
-        for f in os.listdir(parent_dir)
-        if os.path.isdir(os.path.join(parent_dir, f))
-        and not (f.startswith("env") or f.startswith("alembic"))
-        and not f.startswith(".")
-    ]
+    env_path = os.path.join(parent_dir, ".env")
+    project_name = None
 
-    if not project_folders:
-        print("No project found. Please create a project first.")
+    if not os.path.exists(env_path):
+        print("No .env file found in the current directory.")
         return
 
-    return os.path.join(parent_dir, project_folders[0])
+    with open(env_path, "r") as f:
+        for line in f:
+            if line.strip().startswith("PROJECT_NAME"):
+                project_name = line.split("=", 1)[-1].strip()
+                break
+    if not project_name:
+        print("PROJECT_NAME not found in .env file.")
+        return
+
+    apps_dir_folder = os.path.join(parent_dir, project_name)
+    if not os.path.isdir(apps_dir_folder):
+        print(f"Apps dirs '{apps_dir_folder}' not found.")
+        return
+
+    return apps_dir_folder
 
 
 def run():
-    project_folder=os.getcwd()
-    main_entry=os.path.join(project_folder,"__main__.py")
-    subprocess.run(["python",main_entry])
+    project_folder =  os.getcwd()
+    main_entry = os.path.join(project_folder, "__main__.py")
+    subprocess.run(["python", main_entry])
+
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: elrahapi <commande> <name>")
         sys.exit(1)
-    if len(sys.argv)>=2:
+    if len(sys.argv) >= 2:
         command = sys.argv[1]
-    if len(sys.argv)>=3:
+    if len(sys.argv) >= 3:
         name = sys.argv[2]
     if command == "run":
         run()
