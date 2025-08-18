ef23152d35eb0922420db30418def4a19393e383
Harlequelrah <degbovimax19@gmail.com>
2025-08-18 01:33:10 +0000

    update to 1.2.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="elrahapi",
-    version="1.2.0.7",
+    version="1.2.0.8",
     packages=find_packages(),
     description="Package de développement d'API basé FastAPI",
     long_description=long_description,
