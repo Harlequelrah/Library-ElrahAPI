4ff1f211c3ac612ba836bc864e84f6fd39fcbe10
Harlequelrah <degbovimax19@gmail.com>
2025-08-18 01:54:38 +0000

    ajout de __init_.py à otp_setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="elrahapi",
-    version="1.2.0.8",
+    version="1.2.0.9",
     packages=find_packages(),
     description="Package de développement d'API basé FastAPI",
     long_description=long_description,
