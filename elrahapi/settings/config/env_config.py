615db9b56eea2978a7a60c05eb384b01a544a2fa
Harlequelrah <degbovimax19@gmail.com>
2026-06-07 17:11:28 +0000

    update Settings

--- elrahapi/settings/config/env_config.py
+++ elrahapi/settings/config/env_config.py
@@ -1,10 +1,10 @@
-from pydantic import ConfigDict
+from pydantic_settings import SettingsConfigDict
 
 from elrahapi.elrahsettings.models import ElrahSettings
 
 
 class Settings(ElrahSettings):
-    model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
