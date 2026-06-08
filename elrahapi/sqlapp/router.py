a3ed790956cc14af377c6eb7fb33dab992e19489
Harlequelrah <degbovimax19@gmail.com>
2026-06-08 22:59:20 +0000

    retrait des mentions d'authentification dans router de app

--- elrahapi/sqlapp/router.py
+++ elrahapi/sqlapp/router.py
@@ -1,5 +1,4 @@
 from app.myapp.cruds import myapp_crud
-from app.settings.config.auth_config import authentication
 from elrahapi.router.router_namespace import DefaultRoutesName, TypeRoute
 from elrahapi.router.router_provider import CustomRouterProvider
 
@@ -7,8 +6,6 @@ router_provider = CustomRouterProvider(
     prefix="/items",
     tags=["item"],
     crud=myapp_crud,
-    # authentication=authentication,
 )
 
 myapp_router = router_provider.get_public_router()
-# myapp_router = router_provider.get_protected_router()
